afdd19dbb5510633a92b3f399ea7d3fe73ae2c47
chenhao <chenhao@youzan.com>
2017-11-21 21:31:19 +0800

    sync

--- setup.py
+++ setup.py
@@ -6,34 +6,35 @@
 from setuptools import setup, find_packages
 import tools
 
-long_description = """ long desc """
+long_description = """ xiaowa's python tools """
 
 setup(
-    name='bigdata-tools',
-    version=tools.__version__,
-    description='history-flush',
-    long_description=long_description,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.2",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Topic :: Documentation",
-    ],
-    keywords='py-tool',
-    author='jerrychen',
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'history-flush = tools.cmds.history_flush:command_line_runner',
+        name='bigdata-tools',
+        version=tools.__version__,
+        description='history-flush',
+        long_description=long_description,
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "Environment :: Console",
+            "Intended Audience :: Developers",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.2",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+            "Topic :: Documentation",
+        ],
+        keywords='py-tool',
+        author='jerrychen',
+        packages=find_packages(),
+        entry_points={
+            'console_scripts': [
+                'history-flush = tools.cmds.history_flush:command_line_runner',
+                'kafka-produce = tools.cmds.kafka_produce:command_line_runner'
+            ]
+        },
+        install_requires=[
         ]
-    },
-    install_requires=[
-    ]
 )
---
+++ tools/cmds/kafka_produce.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 11/21/17 6:19 PM
+# @Author  : xiaowa
+
+
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+import argparse
+import codecs
+
+
+def send_message(topic, message, server):
+    if not server:
+        server = "127.0.0.1:9092"
+    producer = KafkaProducer(bootstrap_servers=[server])
+    # print(type(message))
+    if type(message) == str:
+        message = message.encode("utf8")
+
+    future = producer.send(topic, message)
+
+    try:
+        future.get(timeout=10)
+        print("message:{} sent".format(message))
+    except KafkaError as e:
+        print(e)
+
+
+def do_produce(topic, file, expression, server, **kwargs):
+    if not expression:
+        message = codecs.open(file, "r", "utf8").read()
+    else:
+        message = expression
+    send_message(server=server, topic=topic, message=message)
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='kafka produce')
+    # parser.add_argument('cmd', metavar='command', type=str,
+    #                     help='the command to execute\nproduce/consume')
+    parser.add_argument('-s', '--server', type=str,
+                        help='the kafka boot_strap address')
+
+    parser.add_argument('-t', '--topic', type=str,
+                        help='the kafka topic')
+    parser.add_argument('-f', '--file', type=str,
+                        help='the message file')
+    parser.add_argument('-e', '--expression', type=str,
+                        help='the message')
+
+    return parser
+
+
+def command_line_runner():
+    parser = get_parser()
+    args = vars(parser.parse_args())
+    do_produce(**args)
+
+
+if __name__ == '__main__':
+    command_line_runner()
